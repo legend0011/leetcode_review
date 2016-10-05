a640efc1227f18138362bcc544ebe30cfabef6ff
Xiangming Wang <washiwxm@gmail.com>
2016-10-05 18:48:37 +0800

    minimum-path-sum

---
+++ minimum-path-sum.cc
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/minimum-path-sum/
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if (grid.empty()) return 0;
+        int m = grid.size(), n = grid[0].size();
+        for (int i = 1; i < n; ++i)
+            grid[0][i] = grid[0][i-1] + grid[0][i];
+        for (int j = 1; j < m; ++j)
+            grid[j][0] = grid[j-1][0] + grid[j][0];
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j)
+                grid[i][j] = min(grid[i-1][j], grid[i][j-1]) + grid[i][j];
+        }
+        return grid[m-1][n-1];
+    
+    }
+};
