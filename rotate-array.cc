4c820884c21c668efd968d54a78180364e6566c8
Xiangming Wang <washiwxm@gmail.com>
2016-10-10 22:06:02 +0800

    rotate-array

---
+++ rotate-array.cc
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/rotate-array/
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+  		reverse(nums.begin(), nums.end());
+		reverse(nums.begin(), nums.begin() + k % nums.size());      
+		reverse(nums.begin() + k % nums.size(), nums.end());      
+    }
+};
